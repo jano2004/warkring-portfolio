fba7d1d69f73223875dff6a9ac34219c70c00423
Lukas Krings <lukaskrings@lukass-air.speedport.ip>
2024-03-07 00:18:13 +0100

    empty input fields

--- portfolio/src/contact/contactPage.jsx
+++ portfolio/src/contact/contactPage.jsx
@@ -1,9 +1,14 @@
-import React, { useRef } from 'react';
+import React, { useRef, useState } from 'react';
 import emailjs from "@emailjs/browser";
 import './contactPage.css';
 
 function ContactPage() {
     const form = useRef();
+    const [formData, setFormData] = useState({
+        user_name: '',
+        user_email: '',
+        message: ''
+    });
 
     const sendEmail = (e) => {
         e.preventDefault();
@@ -25,14 +30,34 @@ function ContactPage() {
                 }
             );
     };
+
+    const handleInputChange = (e) => {
+        const { name, value } = e.target;
+        setFormData({
+            ...FormData,
+            [name]: value
+        });
+    };
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        sendEmail(e);
+        setFormData({
+            user_name: '',
+            user_email: '',
+            message: ''
+        });
+    };
+
+
     return (
-        <form ref={form} onSubmit={sendEmail}>
+        <form ref={form} onSubmit={handleSubmit}>
             <label>Name</label>
-            <input type="text" name="user_name" />
+            <input type="text" name="user_name" value={formData.user_name} onChange={handleInputChange} />
             <label>Email</label>
-            <input type="email" name="user_email" />
+            <input type="email" name="user_email" value={formData.user_email} onChange={handleInputChange} />
             <label>Nachricht</label>
-            <textarea name="message" />
+            <textarea name="message" value={formData.message} onChange={handleInputChange} />
             <input type="submit" value="Send" />
         </form>
     );
