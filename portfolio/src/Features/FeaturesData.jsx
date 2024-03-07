37094d557fa2fe188a6c3c99cd4de8186e85ef89
Nick <nick.poettgen@web.de>
2024-03-07 17:26:45 +0100

    added features to feature folder

---
+++ portfolio/src/Features/FeaturesData.jsx
@@ -0,0 +1,20 @@
+export const features = [
+    {featureName: 'Benutzerfreundlichkeit', featureType: 'Basic'},
+    {featureName: 'Schnelle Ladezeit', featureType: 'Basic'},
+    {featureName: 'Sicherheit', featureType: 'Basic'},
+    {featureName: 'SEO', featureType: 'Basic'},
+    {featureName: 'Qualitätsinhalte', featureType: 'Basic'},
+    {featureName: 'Kontaktinformationen', featureType: 'Basic'},
+    {featureName: 'Social-Media Integration', featureType: 'Basic'},
+    {featureName: 'Analyse und Feedback', featureType: 'Basic'},
+    {featureName: 'Datenschutz und Rechtliches', featureType: 'Basic'},
+    {featureName: 'Branding', featureType: 'Basic'},
+
+    {featureName: 'Responsive Design', featureType: 'Advanced'},
+    {featureName: 'Call-to-Action', featureType: 'Advanced'},
+    {featureName: 'Visuelle Elemente', featureType: 'Advanced'},
+    {featureName: 'Hohe Inhaltsqualität', featureType: 'Advanced'},
+
+    {featureName: 'Kontakt-Automatisierung', featureType: 'Premium'},
+    {featureName: 'Kauf-Automatisierung', featureType: 'Premium'}
+];
--- portfolio/src/Services/ServiceData.jsx
+++ portfolio/src/Services/ServiceData.jsx
@@ -1,25 +1,4 @@
-export let serviceData = new Array(2);
-
-const features = [
-    {featureName: 'Benutzerfreundlichkeit', featureType: 'Basic'},
-    {featureName: 'Schnelle Ladezeit', featureType: 'Basic'},
-    {featureName: 'Sicherheit', featureType: 'Basic'},
-    {featureName: 'SEO', featureType: 'Basic'},
-    {featureName: 'Qualitätsinhalte', featureType: 'Basic'},
-    {featureName: 'Kontaktinformationen', featureType: 'Basic'},
-    {featureName: 'Social-Media Integration', featureType: 'Basic'},
-    {featureName: 'Analyse und Feedback', featureType: 'Basic'},
-    {featureName: 'Datenschutz und Rechtliches', featureType: 'Basic'},
-    {featureName: 'Branding', featureType: 'Basic'},
-
-    {featureName: 'Responsive Design', featureType: 'Advanced'},
-    {featureName: 'Call-to-Action', featureType: 'Advanced'},
-    {featureName: 'Visuelle Elemente', featureType: 'Advanced'},
-    {featureName: 'Hohe Inhaltsqualität', featureType: 'Advanced'},
-
-    {featureName: 'Kontakt-Automatisierung', featureType: 'Premium'},
-    {featureName: 'Kauf-Automatisierung', featureType: 'Premium'}
-];
+import {features} from "../Features/FeaturesData";
 
 const basicFeatures = features.filter(feature => feature.featureType === 'Basic');
 const advancedFeatures = features.filter(feature => feature.featureType === 'Basic'|| feature.featureType === 'Advanced');
@@ -28,6 +7,9 @@ const premiumFeatures = features.filter(feature => feature.featureType === 'Basi
 const descriptionBasic = 'Für einen einfachen, professionellen Online-Auftritt. Es bietet eine grundlegende Präsenz im Internet, die es Ihnen ermöglicht, ihre Markenidentität zu etablieren und potenzielle Kunden zu erreichen.';
 const descriptionAdvanced = 'Ein professioneller Online-Auftritt, der über das Grundlegende hinausgeht. Die Funktionspalette verfeinert Ihre Präsenz im Internet und schafft zudem eine umfänglich ansprechende Platform für Ihre Kunden. Eine Optimierte Darstellung in allen Suchmaschinen.';
 const descriptionPremium = 'Das Rundum-sorglos-Paket. Durch modernste Funktionen, sowie hochwertigem Design bieten Sie Ihrem Unternehmen die perfekte Präsenz im Internet. Die einfache Bedienung und Verknüpfung ermöglicht es Ihnen professionell und schnell mit Ihren Kunden zu interagieren. Maßgeschneidertes Webdesign und Sichtbarkeit in allen Suchmaschinen für optimiertes Marketing.';
+
+
+export let serviceData = new Array(2);
 
 serviceData[0] =  {packName: 'BASIC | MODERNE UNTERNEHMENS-WEBSITE', packFeatures: basicFeatures, packPrice: '399,- €', packDescription: descriptionBasic};
 serviceData[1] = {packName: 'ADVANCDED | PROFFESSIONELLE UNTERNEHMENS-WEBSITE', packFeatures: advancedFeatures, packPrice: '899,- €', packDescription: descriptionAdvanced};
