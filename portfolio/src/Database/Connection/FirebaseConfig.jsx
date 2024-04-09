554ef869c1be562222f11a2ac502911475d4eedc
jano2004 <wagnerjan463@gmail.com>
2024-04-09 11:43:38 +0200

    firestore package

---
+++ portfolio/src/Database/Connection/FirebaseConfig.jsx
@@ -0,0 +1,12 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+    apiKey: "AIzaSyAyS41uD17F7i286aiKqoV-fkIcu6v2ed8",
+    authDomain: "portfolio-91949.firebaseapp.com",
+    projectId: "portfolio-91949",
+    storageBucket: "portfolio-91949.appspot.com",
+    messagingSenderId: "194298962321",
+    appId: "1:194298962321:web:a890a93157bcf40126ba30"
+};
+
+const app = initializeApp(firebaseConfig);
