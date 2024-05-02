8207a00c0c8af454d6f9718b122c1d615b7fe3b9
jano2004 <wagnerjan463@gmail.com>
2024-05-02 22:25:34 +0200

    first-contact-button

--- portfolio/src/View/Mobile/Main/Main.jsx
+++ portfolio/src/View/Mobile/Main/Main.jsx
@@ -1,6 +1,5 @@
 import Header from "./Header/Header";
 import MainBody from "./MainBody/MainBody";
-import Cardslide from './Cardslide/Main';
 import Menu from './Menu/Menu'
 import React, { useState } from "react";
 import './Main.css';
@@ -8,18 +7,28 @@ import './Main.css';
 function MobileMain() {
     const [menu, setMenu] = useState(false);
     const handleMenuMobileClick= () => {
-        setMenu(!menu);
+        //setMenu(!menu);
     }
     return (
         <div className='MobileBody' onClick={menu ? handleMenuMobileClick : null}>
             <Header handleMenuMobileClick={handleMenuMobileClick}/>
             <div className={'mobileMenu'}>
-                {menu && <Menu />}
+                {menu && <Menu/>}
             </div>
-            <MainBody />
-            <Cardslide />
+            <MainBody/>
+            <p className={'beta_text'}>Wir befinden uns derzeit noch im Aufbau.
+                Schreibe uns eine E-Mail um der Erste zu sei,
+                der die neue Website begrüßen kann
+            </p>
+            <button className={'first_contact_button'} onClick={Contact}>Jetzt kontaktieren</button>
         </div>
     );
+}
+
+function Contact() {
+    let email = "kontakt@warkring.org";
+    let subject = encodeURIComponent("Kontaktanfrage von: {Ihren Namen eintragen}");
+    window.location.href = `mailto:${email}?subject=${subject}`;
 }
 
 export default MobileMain;
