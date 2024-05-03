c8a2ed660b0f6f931ff96b0b7f302c6dae6bcfe7
jano2004 <wagnerjan463@gmail.com>
2024-05-03 13:20:42 +0200

    added beta status

--- portfolio/src/View/Mobile/Main/Main.jsx
+++ portfolio/src/View/Mobile/Main/Main.jsx
@@ -5,30 +5,56 @@ import React, { useState } from "react";
 import './Main.css';
 
 function MobileMain() {
+    const beta = true;
     const [menu, setMenu] = useState(false);
     const handleMenuMobileClick= () => {
         setMenu(!menu);
     }
-    return (
-        <div className='MobileBody' onClick={menu ? handleMenuMobileClick : null}>
-            <Header handleMenuMobileClick={handleMenuMobileClick}/>
-            <div className={'mobileMenu'}>
-                {menu && <Menu/>}
+    if (beta) {
+        return (
+            <div className='MobileBody' onClick={menu ? handleMenuMobileClick : null}>
+                <Header handleMenuMobileClick={handleMenuMobileClick}/>
+                <div className={'mobileMenu'}>
+                    {menu && <Menu/>}
+                </div>
+                <MainBody/>
+                <BetaText />
+                <BetaButton />
             </div>
-            <MainBody/>
-            <p className={'beta_text'}>Wir befinden uns derzeit noch im Aufbau.
-                Schreibe uns eine E-Mail um der Erste zu sei,
-                der die neue Website begrüßen kann
-            </p>
-            <button className={'first_contact_button'} onClick={Contact}>Jetzt kontaktieren</button>
-        </div>
-    );
-}
+        );
+    } else {
+        return (
+            <div className='MobileBody' onClick={menu ? handleMenuMobileClick : null}>
+                <Header handleMenuMobileClick={handleMenuMobileClick}/>
+                <div className={'mobileMenu'}>
+                    {menu && <Menu/>}
+                </div>
+                <MainBody/>
+            </div>
+        );
+    }
 
-function Contact() {
-    let email = "kontakt@warkring.org";
-    let subject = encodeURIComponent("Kontaktanfrage von: {Ihren Namen eintragen}");
-    window.location.href = `mailto:${email}?subject=${subject}`;
 }
 
 export default MobileMain;
+
+function BetaText() {
+    return (
+        <p className={'beta_text'}>
+            Wir befinden uns derzeit noch im Aufbau.
+            Schreibe uns eine E-Mail um der Erste zu sei,
+            der die neue Website begrüßen kann
+        </p>
+    )
+}
+
+function BetaButton() {
+    const Contact = () => {
+        let email = "kontakt@warkring.org";
+        let subject = encodeURIComponent("Kontaktanfrage von: {Ihren Namen eintragen}");
+        window.location.href = `mailto:${email}?subject=${subject}`;
+    }
+    return (
+        <button className={'first_contact_button'} onClick={Contact}>Jetzt kontaktieren</button>
+    )
+}
